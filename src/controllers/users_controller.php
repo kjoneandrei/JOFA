312952b8a75fa6a0013297607775f87c9a6901c3
Andrei Hogea <andy_goal2007@yahoo.com>
2016-05-21 14:03:35 +0200

    picture uploads

--- src/controllers/users_controller.php
+++ src/controllers/users_controller.php
@@ -138,41 +138,41 @@ class UsersController
     public function pictureUpload()
     {
         $id = $_SESSION[USER]->getId();
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]); //name of file changed to be user id
-        $uploadOk = 1;
-        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-    // check if image file is a actual image or fake image
-        if (isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if ($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $uploadOk = 1;
-            } else {
-                echo "File is not an image.";
-                $uploadOk = 0;
-            }
-        }
+        $imageFileType = pathinfo($_FILES['fileToUpload']['name'],PATHINFO_EXTENSION);
 
-// Allow certain file formats
-        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif"
-        ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $uploadOk = 0;
-        }
-// Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded. Much Error somewhere ";
-// if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.Much error such WOW";
+        //if they DID upload a file...
+        if($_FILES['fileToUpload']['name'])
+        {
+            //if no errors...
+            if(!$_FILES['fileToUpload']['error'])
+            {
+                //now is the time to modify the future file name and validate the file
+                $new_file_name = $id; //rename file
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                    && $imageFileType != "gif" ) {
+                    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                    $valid_file = false;
+                }
+                else{
+                    $valid_file = true;
+                }
+                //if the file has passed the test
+                if($valid_file)
+                {
+                    //move it to where we want it to be
+                    move_uploaded_file($_FILES['fileToUpload']['tmp_name'], 'uploads/'.$new_file_name);
+                     echo 'Congratulations!  Your file was accepted.';
+                }
+            }
+            //if there is an error...
+            else
+            {
+                //set that to be the returned message
+                echo 'Ooops!  Your upload triggered the following error:  '.$_FILES['fileToUpload']['error'];
             }
         }
-    }
+        }
+    
 
 
 }
